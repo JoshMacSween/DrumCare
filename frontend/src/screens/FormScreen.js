16803f9cfd8b9876894d7f020c329539b4860aa8
Joshua MacSween <joshmacsween@gmail.com>
2021-01-04 16:43:13 -0700

    styled form

--- frontend/src/screens/FormScreen.js
+++ frontend/src/screens/FormScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 // import NetlifyForm from "react-ssg-netlify-forms";
@@ -7,48 +7,64 @@ import { useHistory } from "react-router";
 
 const FormScreen = () => {
   return (
-    <Form
-      name="contact"
-      method="post"
-      data-netlify="true"
-    >
-      <Form.Control type="hidden" name="form-name" value="contact" />
-      <Form.Control type="text" name="name" placeholder="Name" />
-      <br />
-      <Form.Control type="email" name="email" placeholder="Email" />
-      <br />
-      <Form.Control type="number" name="phone number" placeholder="Phone Number" />
-      <br />
-      <Form.Control type="date" name="date of service" placeholder="preffered date of service" />
-      <Form.Control name="Service Package" as="select">
-        <option>Tuning</option>
-        <option>Bronze Maintenance Package</option>
-        <option>Silver Maintenance Package</option>
-        <option>Gold Maintenance Package</option>
-        <option>Other</option>
-      </Form.Control>
-{/*
-      <Form.Control type="number" name="phoneNumber" />
-      <Form.Control required type="date" name="date" />
-      <Form.Control type="number" name="kitSize" />
-      <Form.Control required name="Service Package" as="select">
-        <option>Tuning</option>
-        <option>Bronze Maintenance Package</option>
-        <option>Silver Maintenance Package</option>
-        <option>Gold Maintenance Package</option>
-        <option>Other</option>
-      </Form.Control>
-      {formValues.package === "Other" ? (
+    <Container className="mt-4">
+      <Form name="contact" method="post" data-netlify="true">
+        <Form.Control type="hidden" name="form-name" value="contact" />
+
         <Form.Group>
-          What would you like to tell us?
-          <Form.Control as="textarea" rows={3} name="inquiryText" />
+          <Row>
+            <Col>
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="name" />
+            </Col>
+            <Col>
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" name="email" />
+            </Col>
+            <Col>
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="number" name="phone number" />
+            </Col>
+          </Row>
         </Form.Group>
-      ) : null} */}
 
-      <Form.Control as="textarea" name="message" />
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Preffered Date of Service</Form.Label>
+              <Form.Control type="date" name="date of service" />
+            </Col>
 
-      <button type="submit">Send</button>
-    </Form>
+            <Col>
+              <Form.Label>Kit Size</Form.Label>
+              <Form.Control name="kit size" type="number" min="1" />
+            </Col>
+          </Row>
+        </Form.Group>
+
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Label>Service Package</Form.Label>
+              <Form.Control name="package" as="select">
+                <option>Tuning</option>
+                <option>Bronze Maintenance Package</option>
+                <option>Silver Maintenance Package</option>
+                <option>Gold Maintenance Package</option>
+                <option>Other</option>
+              </Form.Control>
+            </Col>
+          </Row>
+        </Form.Group>
+
+        <Form.Label>Details</Form.Label>
+        <Form.Control as="textarea" name="message" />
+
+        <Button type="submit" block>
+          Send
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
